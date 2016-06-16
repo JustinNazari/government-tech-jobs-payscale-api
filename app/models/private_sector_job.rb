bc59072e73a33663e330e64243c837bd5bfbc518
JustinNazari <justinnazari@gmail.com>
2016-06-15 20:54:32 -0400

    working section and private sector job models, seeds

---
+++ app/models/private_sector_job.rb
@@ -0,0 +1,2 @@
+class PrivateSectorJob < ApplicationRecord
+end
--- db/migrate/20160615220646_add_private_sector_pay.rb
+++ db/migrate/20160615220646_add_private_sector_pay.rb
@@ -1,6 +1,6 @@
 class AddPrivateSectorPay < ActiveRecord::Migration[5.0]
   def change
-    create_table :private_sector_pay do |t|
+    create_table :private_sector_jobs do |t|
       t.string :title
       t.integer :min
       t.integer :max
