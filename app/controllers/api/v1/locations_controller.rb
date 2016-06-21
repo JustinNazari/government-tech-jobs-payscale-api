fa25bd7d9bcfe1786f3b592ea35cf21a20b357b3
JustinNazari <justinnazari@gmail.com>
2016-06-21 10:35:51 -0400

    location show

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -4,6 +4,10 @@ module Api
       def index
         render json: Location.includes(:public_sector_special_pay_jobs), include: ['public_sector_special_pay_jobs']
       end
+      def show
+        location = Location.find(params[:id])
+        render json: location
+      end
     end
   end
 end
